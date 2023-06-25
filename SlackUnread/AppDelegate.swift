f2c90c7221eebd754a1c1379dff3e7521df78d03
Yanxi Wang <wangyanxi@users.noreply.github.com>
2023-06-25 15:00:55 +0800

    watch slack badge

--- SlackUnread/AppDelegate.swift
+++ SlackUnread/AppDelegate.swift
@@ -6,6 +6,12 @@
 //
 
 import Cocoa
+import OSLog
+import SwiftyAttributes
+
+let logger = Logger(subsystem: "org.woodentable.SlackUnread", category: "menu-bar")
+
+// https://developer.apple.com/design/human-interface-guidelines/the-menu-bar#Menu-bar-extras
 
 class AppDelegate: NSObject, NSApplicationDelegate {
 
@@ -16,14 +22,75 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         self.statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
 
         self.setupMenus()
+        self.changeStatusBarButton(errorMessage: nil, badgeText: nil)
+
+        AXUIElementHelper.requestPermission()
+
+        self.watchSlackBadgeAndUpdateMenuBar()
     }
 
     func setupMenus() {
         let menu = NSMenu()
 
-        menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: ""))
+        let errorHandleItem = NSMenuItem(title: "", action: #selector(self.didTapErrorHandleMenuItem), keyEquivalent: "")
+
+        let quitItem = NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "")
+
+        menu.addItem(errorHandleItem)
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(quitItem)
 
         self.statusItem.menu = menu
+    }
+
+    private func changeStatusBarButton(errorMessage: String?, badgeText: String?) {
+        guard let button = statusItem.button else {
+            return
+        }
+
+        let slackIcon = NSImage(named: "SlackIcon")!
+        slackIcon.isTemplate = true
+
+        button.imagePosition = .imageLeft
+
+        button.appearsDisabled = errorMessage != nil
+        button.image = slackIcon
+
+        button.title = (badgeText == "•" ? "●" : badgeText) ?? ""
+        button.alignment = .right
+
+        if let errorHandleItem = statusItem.menu?.items.first {
+            errorHandleItem.title = errorMessage ?? ""
+            errorHandleItem.isHidden = errorMessage == nil
+        }
+    }
+
+    func watchSlackBadgeAndUpdateMenuBar() {
+        var lastBadgeInfo: (error: String?, badge: String?) = (nil, nil)
+
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+            var currentBadgeInfo: (error: String?, badge: String?) = (nil, nil)
+
+            let enabled = AXUIElementHelper.checkPermission()
+            if enabled {
+                currentBadgeInfo = AXUIElementHelper.badgeLabelOfApplication(appName: "Slack")
+            } else {
+                currentBadgeInfo = ("no permission", nil)
+            }
+
+            if currentBadgeInfo == lastBadgeInfo {
+                return
+            }
+            lastBadgeInfo = currentBadgeInfo
+
+            self.changeStatusBarButton(errorMessage: currentBadgeInfo.error, badgeText: currentBadgeInfo.badge)
+        }
+    }
+
+    @objc func didTapErrorHandleMenuItem() {
+        if !AXUIElementHelper.checkPermission() {
+            AXUIElementHelper.requestPermission()
+        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
